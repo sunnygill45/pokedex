6710a611906f2c6412ef17f47e0464aa1b7136ac
sunnygill45 <sunnygill45@gmail.com>
2022-05-24 11:48:05 +0530

    finished

--- script.js
+++ script.js
@@ -16,3 +16,50 @@ const colors = {
 	fighting: '#E6E0D4',
 	normal: '#F5F5F5'
 }
+
+const main_types = Object.keys(colors)
+
+const fetchPokemons = async () => {
+    for(let i = 1; i <= pokemon_count; i++) {
+        await getPokemon(i)
+    }
+}
+
+const getPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url)
+    const data = await res.json() 
+    createPokeCard(data)
+}
+
+const createPokeCard = (pokemon) => {
+    const pokeEl = document.createElement('div')
+    pokeEl.classList.add('pokemon')
+
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+
+    const id = pokemon.id.toString().padStart(3, '0')
+
+    const poke_types = pokemon.types.map(type => type.type.name)
+    const type = main_types.find(type => poke_types.indexOf(type) > -1)
+    const color = colors[type]
+
+    pokeEl.style.backgroundColor = color
+    
+    const pokemonInnerHTML = `
+    <div class="img-container">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="">
+    </div>
+    <div class="info">
+        <span class="number">#${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">Type: <span>${type}</span></small>
+    </div>
+    `
+
+    pokeEl.innerHTML = pokemonInnerHTML
+
+    poke_container.appendChild(pokeEl)
+}
+
+fetchPokemons()
